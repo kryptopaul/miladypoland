93dfb1b488428eabe12e393b2d2778842d8c4d3f
xisux <87016182+xisux@users.noreply.github.com>
2023-03-26 19:15:38 +0100

    milady poland

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,7 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "solady/utils/SafeTransferLib.sol";
+
 contract Counter {
+
+
+
     uint256 public number;
 
     function setNumber(uint256 newNumber) public {
